9ef72dbfcf0404c8c308d6501528947263270320
Kamil Trzciński <ayufan@ayufan.eu>
2018-02-10 19:56:08 +0100

    do not require sudo

--- inode-bt.sh
+++ inode-bt.sh
@@ -2,6 +2,11 @@
 
 DIR=$(dirname "$0")
 
+if [ $(id -u) -ne 0 ]; then
+  echo "$0: run as root"
+  exit 1
+fi
+
 cd $DIR
-sudo hcitool lescan --passive --duplicates &
-sudo hcidump --raw | ./inode-hcidump.sh
+hcitool lescan --passive --duplicates &
+hcidump --raw | ./inode-hcidump.sh
