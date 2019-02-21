3921f4927a1fa90874d60ee52d5b6e99994b0905
Kamil Trzciński <ayufan@ayufan.eu>
2019-02-21 11:30:24 +0100

    Set proper hostname for WiFi

--- esp32-emeter-analyze/esp32-emeter-analyze.ino
+++ esp32-emeter-analyze/esp32-emeter-analyze.ino
@@ -267,13 +267,15 @@ void connectWifi() {
 
 void setup() {
   Serial.begin(115200);
-  WiFi.mode(WIFI_MODE_STA);
-  WiFi.begin(ssid, password);
 
   char hostname[64];
   uint8_t mac[6];
   WiFi.macAddress(mac);
   sprintf(hostname, "esp32-inode-%02x%02x%02x%02x%02x%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+  WiFi.setHostname(hostname);
+  WiFi.mode(WIFI_MODE_STA);
+  WiFi.begin(ssid, password);
 
   Serial.printf("Compiled: %s %s\n", __DATE__, __TIME__);
   Serial.printf("Device Hostname = %s\n", hostname);
